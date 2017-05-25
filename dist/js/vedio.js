7be8e3191b3d23ba120ce49417650a13e0c1ecc0
guanyafei <1902749059@qq.com>
2017-05-25 17:32:17 +0800

    视频及搜索代码简化

--- dist/js/vedio.js
+++ dist/js/vedio.js
@@ -1,40 +1,62 @@
 'use strict';
 
-$(function () {
-    //单击导航栏标签  样式改变
-    var lis = $("nav ul li");
+//单击导航栏标签  样式改变
+var lis = $("nav ul li");
 
-    var _loop = function _loop(i) {
-        var that = lis[i];
-        $(that).bind('click', function () {
-            $(that).children('a').addClass('active').parent().siblings().children('a').removeClass('active');
-        });
-    };
+var _loop = function _loop(i) {
+    var that = lis[i];
+    $(that).bind('click', function () {
+        $(that).children('a').addClass('active').parent().siblings().children('a').removeClass('active');
+    });
+};
 
-    for (var i = 0; i < lis.length; i++) {
-        _loop(i);
+for (var i = 0; i < lis.length; i++) {
+    _loop(i);
+}
+
+//窗口卷去部分大于banner偏移量 返回图标展示
+$(window).scroll(function () {
+    if (document.body.scrollTop > $(".box").offset().top) {
+        $(".back").fadeIn("slow");
+    } else {
+        $(".back").fadeOut("normal");
     }
-
-    //窗口卷去部分大于banner偏移量 返回图标展示
-    $(window).scroll(function () {
-        if (document.body.scrollTop > $(".box").offset().top) {
-            $(".back").fadeIn("slow");
-        } else {
-            $(".back").fadeOut("normal");
+});
+//代理  点击播放按钮 视频播放
+$(".content").delegate(".playBtn", "click", function () {
+    $("video").each(function (idx, ele) {
+        if (!ele.paused) {
+            ele.load();
+            setVedioTime();
+            $(ele).siblings("img,span").fadeIn();
         }
     });
-
-    //设置视频高度与遮罩图片高度一致  防止样式错乱
-    $("video").height($("img.img-responsive").height());
-    //点击播放按钮 视频播放
-    $(".playBtn").click(function () {
-        $("video").each(function (idx, ele) {
-            if (!ele.paused) {
-                ele.load();
-                $(this).parent().children("img,span").fadeIn();
-            }
-        });
-        $(this).parent().children("img,span").fadeOut();
-        $(this).parent().children("video")[0].play();
-    });
+    $(this).parent().children("img,span").fadeOut();
+    $(this).parent().children("video")[0].play();
 });
+//视频加载后触发  获取视频长度并设置
+var setVedioTime = function setVedioTime() {
+    $("video").each(function (idx, ele) {
+        var vLength = ele.duration;
+        if (vLength > 0) {
+            //小时  
+            var h = parseInt(vLength / (60 * 60));
+            //分
+            var m = parseInt(vLength % (60 * 60) / 60);
+            //秒
+            var s = parseInt(vLength % 60);
+
+            if (h < 10) {
+                h = '0' + h;
+            }
+            if (m < 10) {
+                m = '0' + m;
+            }
+            if (s < 10) {
+                s = '0' + s;
+            }
+            var time = h === '00' ? m + ':' + s : h + ':' + m + ':' + s;
+            $(ele).siblings(".badge").html(time);
+        }
+    });
+};
--- src/js/vedio.js
+++ src/js/vedio.js
@@ -1,33 +1,55 @@
-    $(function() {
-        //单击导航栏标签  样式改变
-        var lis = $("nav ul li");
-        for (let i = 0; i < lis.length; i++) {
-            let that = lis[i];
-            $(that).bind('click', () => {
-                $(that).children('a').addClass('active').parent().siblings().children('a').removeClass('active');
-            });
-        }
+  //单击导航栏标签  样式改变
+  var lis = $("nav ul li");
+  for (let i = 0; i < lis.length; i++) {
+      let that = lis[i];
+      $(that).bind('click', () => {
+          $(that).children('a').addClass('active').parent().siblings().children('a').removeClass('active');
+      });
+  }
 
-        //窗口卷去部分大于banner偏移量 返回图标展示
-        $(window).scroll(() => {
-            if (document.body.scrollTop > $(".box").offset().top) {
-                $(".back").fadeIn("slow");
-            } else {
-                $(".back").fadeOut("normal");
-            }
-        });
+  //窗口卷去部分大于banner偏移量 返回图标展示
+  $(window).scroll(() => {
+      if (document.body.scrollTop > $(".box").offset().top) {
+          $(".back").fadeIn("slow");
+      } else {
+          $(".back").fadeOut("normal");
+      }
+  });
+  //代理  点击播放按钮 视频播放
+  $(".content").delegate(".playBtn", "click", function() {
+      $("video").each((idx, ele) => {
+          if (!ele.paused) {
+              ele.load();
+              setVedioTime();
+              $(ele).siblings("img,span").fadeIn();
+          }
+      });
+      $(this).parent().children("img,span").fadeOut();
+      $(this).parent().children("video")[0].play();
+  });
+  //视频加载后触发  获取视频长度并设置
+  var setVedioTime = () => {
+      $("video").each((idx, ele) => {
+          var vLength = ele.duration;
+          if (vLength > 0) {
+              //小时  
+              var h = parseInt(vLength / (60 * 60));
+              //分
+              var m = parseInt((vLength % (60 * 60)) / 60);
+              //秒
+              var s = parseInt(vLength % 60);
 
-        //设置视频高度与遮罩图片高度一致  防止样式错乱
-        $("video").height($("img.img-responsive").height());
-        //点击播放按钮 视频播放
-        $(".playBtn").click(function() {
-            $("video").each(function(idx,ele) {
-                if (!ele.paused) {
-                    ele.load();
-                    $(this).parent().children("img,span").fadeIn();
-                }
-            });
-            $(this).parent().children("img,span").fadeOut();
-            $(this).parent().children("video")[0].play();
-        });
-    });
+              if (h < 10) {
+                  h = '0' + h;
+              }
+              if (m < 10) {
+                  m = '0' + m;
+              }
+              if (s < 10) {
+                  s = '0' + s;
+              }
+              var time=h === '00' ? m + ':' + s : h + ':' + m + ':' + s;
+              $(ele).siblings(".badge").html(time);
+          }
+      });
+  }

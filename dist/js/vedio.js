5fafe61c630f2030b93a6a0375ed33a3f5cd24e3
guanyafei <1902749059@qq.com>
2017-06-08 17:27:51 +0800

    视频控制条显示问题修改及优化

--- dist/js/vedio.js
+++ dist/js/vedio.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var flag = false; //控制条是否显示标志
+var flag = false;
 //单击导航栏标签  样式改变
 var lis = $("nav ul li");
 
@@ -26,22 +26,28 @@ $(window).scroll(function () {
 //代理  点击播放按钮 视频播放
 $(".content").delegate(".playBtn", "click", function () {
     $("video").each(function (idx, ele) {
-        if (!ele.paused) {
+        ele.tag = true; //控制条显现标志   false隐藏
+        if (!ele.paused || ele.currentTime != 0) {
+            $(ele).parentsUntil(".box").find(".VController").animate({ bottom: '-44px', opacity: '0' }, 0);
             ele.load();
-            $(ele).parentsUntil(".box").children(".middle").css("backgroundColor", "white");
-            setVedioTime();
+            //修改控制条标志 控制条隐藏
+            ele.tag = false;
             $(ele).siblings("img,span").fadeIn();
             $(ele).parentsUntil(".box").find(".play").toggleClass("pause");
         }
     });
-    $(this).parentsUntil(".box").children(".middle").css("backgroundColor", "black");
     $(this).parent().children("img,span").fadeOut();
     $(this).parent().children("video")[0].play();
+    ctrPlayBtn($(this).parentsUntil(".box").find(".play").get(0));
+    ctrSoundBtn($(this).parentsUntil(".box").find(".sound").get(0));
+    ctrProgress($(this).parentsUntil(".box").find(".progress").get(0));
+    ctrFullscreenBtn($(this).parentsUntil(".box").find(".fullscreen").get(0));
+    ctrShuBtn($(this).parentsUntil(".box").find(".shu").get(0));
     onVideoPlay($(this).parent().children("video")[0]);
     $(this).parentsUntil(".box").find(".play").toggleClass("pause");
-    flag = true;
-    controllerOpt($(this).parent().children("video")[0], flag);
+    controllerOpt($(this).parent().children("video")[0]);
 });
+
 //视频加载后触发  获取视频长度并设置
 var setVedioTime = function setVedioTime() {
     $("video").each(function (idx, ele) {
@@ -51,6 +57,7 @@ var setVedioTime = function setVedioTime() {
         $(ele).parentsUntil(".box").find(".totalTime").html(time);
     });
 };
+
 //格式化视频总时间
 var formatTime = function formatTime(ele, vLength) {
     if (vLength > 0) {
@@ -76,24 +83,21 @@ var formatTime = function formatTime(ele, vLength) {
 };
 //自定义视频控制按钮
 // 点击播放按钮  视频播放
-
-$(".play").each(function (idx, ele) {
+var ctrPlayBtn = function ctrPlayBtn(ele) {
     $(ele).click(function () {
         if ($(ele).parentsUntil(".box").find(".play").hasClass("pause")) {
             $(ele).parentsUntil(".box").find(".play").toggleClass("pause");
-            flag = false;
             $(ele).parentsUntil(".box").find("video")[0].pause();
-            $(ele).parentsUntil(".box").find(".playBtn").fadeIn();
         } else {
             $(ele).parentsUntil(".box").find("video")[0].play();
-            flag = true;
             onVideoPlay($(ele).parentsUntil(".box").find("video")[0]);
             $(ele).parentsUntil(".box").find(".play").toggleClass("pause");
         }
     });
-});
+};
+
 // 音量按钮设置
-$(".sound").each(function (idx, ele) {
+var ctrSoundBtn = function ctrSoundBtn(ele) {
     $(ele).click(function () {
         if ($(ele).parentsUntil(".box").find(".sound").hasClass("mute")) {
             $(ele).parentsUntil(".box").find(".sound").toggleClass("mute");
@@ -103,7 +107,7 @@ $(".sound").each(function (idx, ele) {
             $(ele).parentsUntil(".box").find(".sound").toggleClass("mute");
         }
     });
-});
+};
 
 //当前的播放位置发送改变时触发 改变进度条
 var onVideoPlay = function onVideoPlay(videoDom) {
@@ -122,15 +126,16 @@ var onVideoPlay = function onVideoPlay(videoDom) {
 };
 
 //点击进度条  跳转到播放位置
-$(".progress").each(function (idx, ele) {
+var ctrProgress = function ctrProgress(ele) {
     $(ele).click(function (e) {
         var percent = e.offsetX / ele.offsetWidth * 100 + '%';
         $(ele).children('.progress-bar').width(percent);
         $(ele).parentsUntil(".box").find("video")[0].currentTime = $(ele).parentsUntil(".box").find("video")[0].duration * e.offsetX / ele.offsetWidth;
     });
-});
+};
+
 //放大
-$(".fullscreen").each(function (idx, ele) {
+var ctrFullscreenBtn = function ctrFullscreenBtn(ele) {
     $(ele).click(function () {
         var video = $(ele).parentsUntil(".box").find("video").get(0);
         if (video.requestFullscreen) {
@@ -145,23 +150,24 @@ $(".fullscreen").each(function (idx, ele) {
             video.mozRequestFullScreen();
         }
     });
-});
+};
+
 //触摸video区域  控制条显示  5秒后消失
-var controllerOpt = function controllerOpt(ele, flag) {
+var controllerOpt = function controllerOpt(ele) {
     $(ele).on('touchstart', function () {
-        if (flag) {
-            $(ele).parent().next(".VController").animate({ bottom: '0' }, "slow");
+        if (ele.tag) {
+            $(ele).parent().next(".VController").animate({ bottom: '0', opacity: '1' }, "slow");
         }
     });
     $(ele).on('touchend', function () {
         setTimeout(function () {
-            $(ele).parent().next(".VController").animate({ bottom: '-44px' }, "slow");
+            $(ele).parent().next(".VController").animate({ bottom: '-44px', opacity: '0' }, "slow");
         }, 1000000);
     });
 };
 
 //拖动竖条快进或后退视频
-$(".shu").each(function (idx, ele) {
+var ctrShuBtn = function ctrShuBtn(ele) {
     //手指触摸位置
     var startX = 0;
     //手指移动距离
@@ -179,22 +185,21 @@ $(".shu").each(function (idx, ele) {
         $(ele).parentsUntil(".box").find("video")[0].pause();
         //计算移动距离
         moveX = e.touches[0].clientX - startX;
-        distanceX = moveX;
         //修改竖条移动距离
         //progress-bar当前宽度
         var currentWidth = $(ele).prev(".progress").children(".progress-bar").width();
         //progress宽度
         var progressWidth = $(ele).prev(".progress").width();
-        distanceX = currentWidth + moveX;
-        var percent = distanceX / progressWidth * 100 + '%';
+        var percent = (distanceX + moveX) / progressWidth * 100 + '%';
         $(ele).prev(".progress").children(".progress-bar").width(percent);
         // 竖条位置
         ele.style.left = $(ele).parentsUntil(".box").find(".progress-bar").width() + "px";
-        $(ele).parentsUntil(".box").find("video")[0].currentTime = $(ele).parentsUntil(".box").find("video")[0].duration * distanceX / progressWidth;
+        $(ele).parentsUntil(".box").find("video")[0].currentTime = $(ele).parentsUntil(".box").find("video")[0].duration * (distanceX + moveX) / progressWidth;
     });
     //触摸结束
     $(ele).on('touchend', function () {
+        distanceX = distanceX + moveX;
         //触摸结束 恢复播放
         $(ele).parentsUntil(".box").find("video")[0].play();
     });
-});
+};
--- src/js/vedio.js
+++ src/js/vedio.js
@@ -1,193 +1,198 @@
-  var flag = false; //控制条是否显示标志
-  //单击导航栏标签  样式改变
-  var lis = $("nav ul li");
-  for (let i = 0; i < lis.length; i++) {
-      let that = lis[i];
-      $(that).bind('click', () => {
-          $(that).children('a').addClass('active').parent().siblings().children('a').removeClass('active');
-      });
-  }
+var flag = false;
+//单击导航栏标签  样式改变
+var lis = $("nav ul li");
+for (let i = 0; i < lis.length; i++) {
+    let that = lis[i];
+    $(that).bind('click', () => {
+        $(that).children('a').addClass('active').parent().siblings().children('a').removeClass('active');
+    });
+}
 
-  //窗口卷去部分大于banner偏移量 返回图标展示
-  $(window).scroll(() => {
-      if (document.body.scrollTop > $(".box").offset().top) {
-          $(".back").fadeIn("slow");
-      } else {
-          $(".back").fadeOut("normal");
-      }
-  });
-  //代理  点击播放按钮 视频播放
-  $(".content").delegate(".playBtn", "click", function() {
-      $("video").each((idx, ele) => {
-          if (!ele.paused) {
-              ele.load();
-              $(ele).parentsUntil(".box").children(".middle").css("backgroundColor","white");
-              setVedioTime();
-              $(ele).siblings("img,span").fadeIn();
-              $(ele).parentsUntil(".box").find(".play").toggleClass("pause");
-          }
-      });
-      $(this).parentsUntil(".box").children(".middle").css("backgroundColor","black");
-      $(this).parent().children("img,span").fadeOut();
-      $(this).parent().children("video")[0].play();
-      onVideoPlay($(this).parent().children("video")[0]);
-      $(this).parentsUntil(".box").find(".play").toggleClass("pause");
-      flag = true;
-      controllerOpt($(this).parent().children("video")[0], flag);
-  });
-  //视频加载后触发  获取视频长度并设置
-  var setVedioTime = () => {
-          $("video").each((idx, ele) => {
-              var vLength = ele.duration;
-              var time = formatTime(ele, vLength);
-              $(ele).siblings(".badge").html(time);
-              $(ele).parentsUntil(".box").find(".totalTime").html(time);
-          });
-      }
-      //格式化视频总时间
-  var formatTime = (ele, vLength) => {
-      if (vLength > 0) {
-          //小时  
-          var h = parseInt(vLength / (60 * 60));
-          //分
-          var m = parseInt((vLength % (60 * 60)) / 60);
-          //秒
-          var s = parseInt(vLength % 60);
+//窗口卷去部分大于banner偏移量 返回图标展示
+$(window).scroll(() => {
+    if (document.body.scrollTop > $(".box").offset().top) {
+        $(".back").fadeIn("slow");
+    } else {
+        $(".back").fadeOut("normal");
+    }
+});
+//代理  点击播放按钮 视频播放
+$(".content").delegate(".playBtn", "click", function() {
+    $("video").each((idx, ele) => {
+        ele.tag = true; //控制条显现标志   false隐藏
+        if (!ele.paused || ele.currentTime != 0) {
+            $(ele).parentsUntil(".box").find(".VController").animate({ bottom: '-44px', opacity: '0' }, 0);
+            ele.load();
+            //修改控制条标志 控制条隐藏
+            ele.tag = false;
+            $(ele).siblings("img,span").fadeIn();
+            $(ele).parentsUntil(".box").find(".play").toggleClass("pause");
+        }
+    });
+    $(this).parent().children("img,span").fadeOut();
+    $(this).parent().children("video")[0].play();
+    ctrPlayBtn($(this).parentsUntil(".box").find(".play").get(0));
+    ctrSoundBtn($(this).parentsUntil(".box").find(".sound").get(0));
+    ctrProgress($(this).parentsUntil(".box").find(".progress").get(0));
+    ctrFullscreenBtn($(this).parentsUntil(".box").find(".fullscreen").get(0));
+    ctrShuBtn($(this).parentsUntil(".box").find(".shu").get(0));
+    onVideoPlay($(this).parent().children("video")[0]);
+    $(this).parentsUntil(".box").find(".play").toggleClass("pause");
+    controllerOpt($(this).parent().children("video")[0]);
+});
 
-          if (h < 10) {
-              h = '0' + h;
-          }
-          if (m < 10) {
-              m = '0' + m;
-          }
-          if (s < 10) {
-              s = '0' + s;
-          }
-          var time = h === '00' ? m + ':' + s : h + ':' + m + ':' + s;
-          return time;
-      }
-  };
-  //自定义视频控制按钮
-  // 点击播放按钮  视频播放
+//视频加载后触发  获取视频长度并设置
+var setVedioTime = () => {
+    $("video").each((idx, ele) => {
+        var vLength = ele.duration;
+        var time = formatTime(ele, vLength);
+        $(ele).siblings(".badge").html(time);
+        $(ele).parentsUntil(".box").find(".totalTime").html(time);
+    });
+}
 
-  $(".play").each((idx, ele) => {
-      $(ele).click(() => {
-          if ($(ele).parentsUntil(".box").find(".play").hasClass("pause")) {
-              $(ele).parentsUntil(".box").find(".play").toggleClass("pause");
-              flag = false;
-              $(ele).parentsUntil(".box").find("video")[0].pause();
-              $(ele).parentsUntil(".box").find(".playBtn").fadeIn();
-          } else {
-              $(ele).parentsUntil(".box").find("video")[0].play();
-              flag = true;
-              onVideoPlay($(ele).parentsUntil(".box").find("video")[0]);
-              $(ele).parentsUntil(".box").find(".play").toggleClass("pause");
-          }
-      });
-  });
-  // 音量按钮设置
-  $(".sound").each((idx, ele) => {
-      $(ele).click(() => {
-          if ($(ele).parentsUntil(".box").find(".sound").hasClass("mute")) {
-              $(ele).parentsUntil(".box").find(".sound").toggleClass("mute");
-              $(ele).parentsUntil(".box").find("video")[0].muted = false;
-          } else {
-              $(ele).parentsUntil(".box").find("video")[0].muted = true;
-              $(ele).parentsUntil(".box").find(".sound").toggleClass("mute");
-          }
-      });
-  });
+//格式化视频总时间
+var formatTime = (ele, vLength) => {
+    if (vLength > 0) {
+        //小时  
+        var h = parseInt(vLength / (60 * 60));
+        //分
+        var m = parseInt((vLength % (60 * 60)) / 60);
+        //秒
+        var s = parseInt(vLength % 60);
 
-  //当前的播放位置发送改变时触发 改变进度条
-  var onVideoPlay = (videoDom) => {
-      videoDom.ontimeupdate = () => {
-          //设置当前播放时间
-          var vLength = videoDom.currentTime;
-          var time = formatTime(videoDom, vLength);
-          $(videoDom).parentsUntil(".box").find(".runTime").html(time);
-          //进度条进度
-          var percent = videoDom.currentTime / videoDom.duration * 100 + "%";
-          $(videoDom).parentsUntil(".box").find(".progress-bar").width(percent);
-          // 竖条位置
-          var shu = $(videoDom).parentsUntil(".box").find(".shu").get(0);
-          shu.style.left = $(videoDom).parentsUntil(".box").find(".progress-bar").width() + "px";
-      };
-  };
+        if (h < 10) {
+            h = '0' + h;
+        }
+        if (m < 10) {
+            m = '0' + m;
+        }
+        if (s < 10) {
+            s = '0' + s;
+        }
+        var time = h === '00' ? m + ':' + s : h + ':' + m + ':' + s;
+        return time;
+    }
+};
+//自定义视频控制按钮
+// 点击播放按钮  视频播放
+var ctrPlayBtn = (ele) => {
+    $(ele).click(() => {
+        if ($(ele).parentsUntil(".box").find(".play").hasClass("pause")) {
+            $(ele).parentsUntil(".box").find(".play").toggleClass("pause");
+            $(ele).parentsUntil(".box").find("video")[0].pause();
+        } else {
+            $(ele).parentsUntil(".box").find("video")[0].play();
+            onVideoPlay($(ele).parentsUntil(".box").find("video")[0]);
+            $(ele).parentsUntil(".box").find(".play").toggleClass("pause");
+        }
+    });
+};
 
-  //点击进度条  跳转到播放位置
-  $(".progress").each((idx, ele) => {
-      $(ele).click((e) => {
-          var percent = e.offsetX / ele.offsetWidth * 100 + '%';
-          $(ele).children('.progress-bar').width(percent);
-          $(ele).parentsUntil(".box").find("video")[0].currentTime = $(ele).parentsUntil(".box").find("video")[0].duration * e.offsetX / ele.offsetWidth;
-      });
-  });
-  //放大
-  $(".fullscreen").each((idx, ele) => {
-      $(ele).click(() => {
-          var video = $(ele).parentsUntil(".box").find("video").get(0);
-          if (video.requestFullscreen) {
-              video.requestFullscreen();
-          } else if (video.webkitRequestFullScreen) {
-              // 如果 在谷歌里面全屏 
-              // 可以再这里 指定 dom元素的大小
-              video.webkitRequestFullScreen();
-          } else if (video.msRequestFullscreen) {
-              video.msRequestFullscreen();
-          } else if (video.mozRequestFullScreen) {
-              video.mozRequestFullScreen();
-          }
-      });
-  });
-  //触摸video区域  控制条显示  5秒后消失
-  var controllerOpt = (ele, flag) => {
-      $(ele).on('touchstart', () => {
-          if (flag) {
-              $(ele).parent().next(".VController").animate({ bottom: '0' }, "slow");
-          }
-      });
-      $(ele).on('touchend', () => {
-          setTimeout(() => {
-              $(ele).parent().next(".VController").animate({ bottom: '-44px' }, "slow");
-          }, 1000000);
-      });
-  };
+// 音量按钮设置
+var ctrSoundBtn = (ele) => {
+    $(ele).click(() => {
+        if ($(ele).parentsUntil(".box").find(".sound").hasClass("mute")) {
+            $(ele).parentsUntil(".box").find(".sound").toggleClass("mute");
+            $(ele).parentsUntil(".box").find("video")[0].muted = false;
+        } else {
+            $(ele).parentsUntil(".box").find("video")[0].muted = true;
+            $(ele).parentsUntil(".box").find(".sound").toggleClass("mute");
+        }
+    });
+};
 
-  //拖动竖条快进或后退视频
-  $(".shu").each((idx, ele) => {
-      //手指触摸位置
-      var startX = 0;
-      //手指移动距离
-      var moveX = 0;
-      //移动距离
-      var distanceX = 0;
-      // 触摸开始
-      $(ele).on('touchstart', (e) => {
-          //记录触摸位置
-          startX = e.touches[0].clientX;
-      });
-      //触摸中
-      $(ele).on('touchmove', (e) => {
-          //移动期间视频暂停播放
-          $(ele).parentsUntil(".box").find("video")[0].pause();
-          //计算移动距离
-          moveX = e.touches[0].clientX - startX;
-          distanceX = moveX;
-          //修改竖条移动距离
-          //progress-bar当前宽度
-          var currentWidth = $(ele).prev(".progress").children(".progress-bar").width();
-          //progress宽度
-          var progressWidth = $(ele).prev(".progress").width();
-          distanceX = currentWidth + moveX;
-          var percent = distanceX / progressWidth * 100 + '%';
-          $(ele).prev(".progress").children(".progress-bar").width(percent);
-          // 竖条位置
-          ele.style.left = $(ele).parentsUntil(".box").find(".progress-bar").width() + "px";
-          $(ele).parentsUntil(".box").find("video")[0].currentTime = $(ele).parentsUntil(".box").find("video")[0].duration * distanceX / progressWidth;
-      });
-      //触摸结束
-      $(ele).on('touchend', () => {
-          //触摸结束 恢复播放
-          $(ele).parentsUntil(".box").find("video")[0].play();
-      });
-  });
+//当前的播放位置发送改变时触发 改变进度条
+var onVideoPlay = (videoDom) => {
+    videoDom.ontimeupdate = () => {
+        //设置当前播放时间
+        var vLength = videoDom.currentTime;
+        var time = formatTime(videoDom, vLength);
+        $(videoDom).parentsUntil(".box").find(".runTime").html(time);
+        //进度条进度
+        var percent = videoDom.currentTime / videoDom.duration * 100 + "%";
+        $(videoDom).parentsUntil(".box").find(".progress-bar").width(percent);
+        // 竖条位置
+        var shu = $(videoDom).parentsUntil(".box").find(".shu").get(0);
+        shu.style.left = $(videoDom).parentsUntil(".box").find(".progress-bar").width() + "px";
+    };
+};
+
+//点击进度条  跳转到播放位置
+var ctrProgress = (ele) => {
+    $(ele).click((e) => {
+        var percent = e.offsetX / ele.offsetWidth * 100 + '%';
+        $(ele).children('.progress-bar').width(percent);
+        $(ele).parentsUntil(".box").find("video")[0].currentTime = $(ele).parentsUntil(".box").find("video")[0].duration * e.offsetX / ele.offsetWidth;
+    });
+};
+
+//放大
+var ctrFullscreenBtn = (ele) => {
+    $(ele).click(() => {
+        var video = $(ele).parentsUntil(".box").find("video").get(0);
+        if (video.requestFullscreen) {
+            video.requestFullscreen();
+        } else if (video.webkitRequestFullScreen) {
+            // 如果 在谷歌里面全屏 
+            // 可以再这里 指定 dom元素的大小
+            video.webkitRequestFullScreen();
+        } else if (video.msRequestFullscreen) {
+            video.msRequestFullscreen();
+        } else if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen();
+        }
+    });
+};
+
+//触摸video区域  控制条显示  5秒后消失
+var controllerOpt = (ele) => {
+    $(ele).on('touchstart', () => {
+        if (ele.tag) {
+            $(ele).parent().next(".VController").animate({ bottom: '0', opacity: '1' }, "slow");
+        }
+    });
+    $(ele).on('touchend', () => {
+        setTimeout(() => {
+            $(ele).parent().next(".VController").animate({ bottom: '-44px', opacity: '0' }, "slow");
+        }, 1000000);
+    });
+};
+
+//拖动竖条快进或后退视频
+var ctrShuBtn = (ele) => {
+    //手指触摸位置
+    var startX = 0;
+    //手指移动距离
+    var moveX = 0;
+    //移动距离
+    var distanceX = 0;
+    // 触摸开始
+    $(ele).on('touchstart', (e) => {
+        //记录触摸位置
+        startX = e.touches[0].clientX;
+    });
+    //触摸中
+    $(ele).on('touchmove', (e) => {
+        //移动期间视频暂停播放
+        $(ele).parentsUntil(".box").find("video")[0].pause();
+        //计算移动距离
+        moveX = e.touches[0].clientX - startX;
+        //修改竖条移动距离
+        //progress-bar当前宽度
+        var currentWidth = $(ele).prev(".progress").children(".progress-bar").width();
+        //progress宽度
+        var progressWidth = $(ele).prev(".progress").width();
+        var percent = (distanceX + moveX) / progressWidth * 100 + '%';
+        $(ele).prev(".progress").children(".progress-bar").width(percent);
+        // 竖条位置
+        ele.style.left = $(ele).parentsUntil(".box").find(".progress-bar").width() + "px";
+        $(ele).parentsUntil(".box").find("video")[0].currentTime = $(ele).parentsUntil(".box").find("video")[0].duration * (distanceX + moveX) / progressWidth;
+    });
+    //触摸结束
+    $(ele).on('touchend', () => {
+        distanceX = distanceX + moveX;
+        //触摸结束 恢复播放
+        $(ele).parentsUntil(".box").find("video")[0].play();
+    });
+};
